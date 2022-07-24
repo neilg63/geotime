f67319b84c7eefddc746b4c1ce3c6d41415db72b
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-07-24 22:16:19 +0100

    reviewd time options

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,10 @@
 pub const MYSQL_HOST_DEFAULT: &str = "127.0.0.1";
 pub const MYSQL_PORT_DEFAULT: u16 = 3306;
 pub const MYSQL_DB_DEFAULT: &str = "timezonedb";
-pub const MYSQL_PASS_DEFAULT: &str = "password";
+pub const MYSQL_USER_DEFAULT: &str = "timezonedb";
+pub const MYSQL_PASS_DEFAULT: &str = "T0XcJfpIBsFGkUY7";
+// pub const MYSQL_PASS_DEFAULT: &str = "password";
 
-pub const GEONAMES_USERNAME_DEFAULT: &str = "demo";
+pub const GEONAMES_USERNAME_DEFAULT: &str = "serpentinegallery";
+//pub const GEONAMES_USERNAME_DEFAULT: &str = "demo";
 pub const GEONAMES_API_BASE: &str = "http://api.geonames.org";
