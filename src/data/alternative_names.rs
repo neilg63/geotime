356d8969736e45150f3f964f517cb00f3cb14fb1
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-04-15 16:05:50 +0100

    added more notes to README.md

--- src/data/alternative_names.rs
+++ src/data/alternative_names.rs
@@ -2,8 +2,8 @@
 * This list should be much more extensive, but it covers the main cases discovered so far
 * in which Geonames fails to provide localised or traditional / historical name variants
 * for major cities in its name or toponymName attributes.
-* The latter attribute usually contains the official transcribed localised name, but sometimes is only available in international English even if variant is common locally. 
-* This lookup set serves only for post-filtering as geonames picks up all these variants, but does not include them in the results, e.f. q=Madras will match Chennai, but this will not be in the results for capital of Tamil Nadu.
+* The latter attribute usually contains the official transcribed localised name, but sometimes is only available in international English even if the variant is common locally. 
+* This lookup set serves only for post-filtering as geonames usually picks up such variants from the search query, but does not include them in the results, e.f. q=Madras will match Chennai, but this will not be in the results for capital of Tamil Nadu.
 * Ideally this should be migrated to a full database
 */
 pub const ALTERNATIVE_NAMES: [(&'static str, &'static str); 11] = [
