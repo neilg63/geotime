884af9864c3bd1ca3b5f4f105012bb3dbca69543
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-04-15 15:27:35 +0100

    added new /search and /lookup services

--- src/data/alternative_names.rs
+++ src/data/alternative_names.rs
@@ -1,8 +1,10 @@
 /*
-* This list should be much more extensive, but it covers the main cases
-* where Geonames fails to provide either localised or traditional / historical names
-* for major cities in its name or toponymName attributes
-* ideally this should be migrated to a full database
+* This list should be much more extensive, but it covers the main cases discovered so far
+* in which Geonames fails to provide localised or traditional / historical name variants
+* for major cities in its name or toponymName attributes.
+* The latter attribute usually contains the official transcribed localised name, but sometimes is only available in international English even if variant is common locally. 
+* This lookup set serves only for post-filtering as geonames picks up all these variants, but does not include them in the results, e.f. q=Madras will match Chennai, but this will not be in the results for capital of Tamil Nadu.
+* Ideally this should be migrated to a full database
 */
 pub const ALTERNATIVE_NAMES: [(&'static str, &'static str); 11] = [
   ("Madras", "Chennai"),
